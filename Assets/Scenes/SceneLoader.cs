75d367c712e31961044d95d8f546cf6cfd95fec3
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-03-20 03:50:25 +0900

    フォルダ整理。

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RampageCars
+{
+    public class SceneLoader : SingletonBase
+    {
+        public Scene Current { get; private set; }
+
+        Coroutine changeing;
+
+        protected override void Init()
+        {
+            Current = SceneManager.GetActiveScene();
+        }
+
+        public void LoadScene(SceneType scene)
+        {
+            if (changeing == null)
+            {
+                changeing = StartCoroutine(SceneChangeAsync(scene));
+            }
+        }
+
+        IEnumerator SceneChangeAsync(SceneType scene)
+        {
+            var load = SceneManager.LoadSceneAsync(((int)scene), LoadSceneMode.Additive);
+            load.allowSceneActivation = false;
+
+            yield return SceneManager.UnloadSceneAsync(Current);
+            load.allowSceneActivation = true;
+
+            changeing = null;
+        }
+    }
+}
