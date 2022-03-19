ebcbb821c887331dcddc46743cdaf768efa34def
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-03-20 06:16:53 +0900

    親クラスのNotNullも見るようにした

--- Assets/C#/EditorScripts/NotNull/Editor/DisallowPlayModeIfNull.cs
+++ Assets/C#/EditorScripts/NotNull/Editor/DisallowPlayModeIfNull.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Reflection;
 using UnityEditor;
+using System;
+using System.Collections.Generic;
 
 namespace EditorScripts
 {
@@ -10,6 +12,26 @@ namespace EditorScripts
         static DisallowPlayModeIfNull()
         {
             EditorApplication.playModeStateChanged += LogNullError;
+        }
+
+        static IEnumerable<FieldInfo> GetFields(Type t)
+        {
+            if (t == null)
+            {
+                yield break;
+            }
+
+            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+                                 BindingFlags.Static | BindingFlags.Instance |
+                                 BindingFlags.DeclaredOnly;
+            foreach (var f in t.GetFields(flags))
+            {
+                yield return f;
+            }
+            foreach (var f in GetFields(t.BaseType))
+            {
+                yield return f;
+            }
         }
 
         private static void LogNullError(PlayModeStateChange state)
@@ -23,7 +45,7 @@ namespace EditorScripts
 
             foreach (var component in GameObject.FindObjectsOfType<Component>(true))
             {
-                foreach (var field in component.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+                foreach (var field in GetFields(component.GetType()))
                 {
                     var attribute = field.GetCustomAttribute<NotNullAttribute>(true);
 
