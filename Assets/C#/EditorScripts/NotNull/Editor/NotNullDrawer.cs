0dfec9bb2bfe2e39ebf14c36841b957af1a38a10
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-03-25 07:23:59 +0900

    サポート外のものを明記。

--- Assets/C#/EditorScripts/NotNull/Editor/NotNullDrawer.cs
+++ Assets/C#/EditorScripts/NotNull/Editor/NotNullDrawer.cs
@@ -8,35 +8,52 @@ namespace EditorScripts
     [CustomPropertyDrawer(typeof(NotNullAttribute))]
     public class NotNullDrawer : PropertyDrawer
     {
+        enum RequireType
+        {
+            OK,
+            Require,
+            NotSupport
+        }
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            EditorGUI.PropertyField(position, property, label, true);
+            position.y += EditorGUI.GetPropertyHeight(property);
             position.height = base.GetPropertyHeight(property, label);
-            EditorGUI.PropertyField(position, property, label);
-            position.y += position.height;
-            if (IsRequire(property))
+
+
+            switch (IsRequire(property))
             {
-                EditorGUI.HelpBox(position, "Set Value", MessageType.Error);
+                case RequireType.Require:
+                    EditorGUI.HelpBox(position, "Set Value", MessageType.Error);
+                    break;
+                case RequireType.NotSupport:
+                    EditorGUI.HelpBox(position, "Not Support", MessageType.Error);
+                    Debug.LogError("サポート外のNotNullがあります", property.serializedObject.targetObject);
+                    break;
+                default:
+                    break;
             }
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (IsRequire(property))
+            if (IsRequire(property) != RequireType.OK)
             {
-                return base.GetPropertyHeight(property, label) * 2f;
+                return EditorGUI.GetPropertyHeight(property) + base.GetPropertyHeight(property, label);
             }
-            return base.GetPropertyHeight(property, label);
+            return EditorGUI.GetPropertyHeight(property);
         }
 
-        private bool IsRequire(SerializedProperty property)
+        private RequireType IsRequire(SerializedProperty property)
         {
             switch (property.propertyType)
             {
                 case SerializedPropertyType.ObjectReference:
-                    return property.objectReferenceValue == null;
-            }
+                    return property.objectReferenceValue == null ? RequireType.Require : RequireType.OK;
 
-            return false;
+                default:
+                    return RequireType.NotSupport;
+            }
         }
     }
 }
