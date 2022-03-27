d3eb4931c140c2a16f0bc5d15b42c209a5d86bdd
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-03-27 16:03:00 +0900

    基本クラスを継承するようにした。

---
+++ Assets/Objects/Player/PlayerAction/DriftPlayerActionBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using EditorScripts;
+using UnityEngine;
+
+namespace RampageCars
+{
+    public abstract class DriftPlayerActionBase : MonoBehaviour, IPlayerAction
+    {
+        [SerializeField, NotNull]
+        GroundChecker groundChecker;
+        [SerializeField]
+        float attack = 3;
+
+        [Header("抵抗力と回転力")]
+        [SerializeField]
+        float addRelative = 100;
+
+        public abstract float RotatePower { get; }
+
+        Rigidbody rb;
+        public bool CanChange { get; private set; } = true;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+        }
+        private void FixedUpdate()
+        {
+            if (!CanChange)
+            {
+                if (groundChecker.IsGrounded)
+                {
+                    rb.AddRelativeForce(new Vector3(-addRelative, -addRelative, -addRelative));
+
+                    rb.MoveRotation(transform.localRotation * Quaternion.AngleAxis(RotatePower, Vector3.up));
+                }
+            }
+        }
+
+
+        public void Do()
+        {
+            CanChange = false;
+        }
+
+        public void Finish()
+        {
+            CanChange = true;
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+        }
+    }
+}
--- Assets/Objects/Player/PlayerAction/RightDriftPlayerAction.cs
+++ Assets/Objects/Player/PlayerAction/RightDriftPlayerAction.cs
@@ -5,49 +5,10 @@ using EditorScripts;
 
 namespace RampageCars
 {
-    public class RightDriftPlayerAction : MonoBehaviour, IPlayerAction
+    public class RightDriftPlayerAction : DriftPlayerActionBase
     {
-        [SerializeField, NotNull]
-        GroundChecker groundChecker;
-        [Header("抵抗力と回転力")]
-        [SerializeField]
-        float addRelative=100;
         [SerializeField]
         float rotatePower = 5;
-        Rigidbody rb;
-        public bool CanChange { get; private set; } = true;
-
-        private void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-
-        }
-        private void FixedUpdate()
-        {
-            if (!CanChange)
-            {
-                if (groundChecker.IsGrounded)
-                {
-                    rb.AddRelativeForce(new Vector3(-addRelative, -addRelative, -addRelative));
-
-                    rb.MoveRotation(transform.localRotation * Quaternion.AngleAxis(rotatePower, Vector3.up));
-                }
-            }
-        }
-
-
-        public void Do()
-        {
-            CanChange = false;
-        }
-
-        public void Finish()
-        {
-            CanChange = true;
-        }
-
-        public void OnCollisionEnter(Collision collision)
-        {
-        }
+        public override float RotatePower => rotatePower;
     }
 }
