9b08e082123913ac80ed40eaf7d47b431ef3394e
momonga2525 <arst.0525@gmail.com>
2022-04-03 00:37:28 +0900

    タイマーを実装しました。外から制限時間を変更できるようにしています。 残り秒数が０になったら、０のままで止まるようにしています。

---
+++ Assets/C#/Debug/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace RampageCars
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField]
+        float TimeLimit = 180;
+
+        TMP_Text time;
+
+        void Start()
+        {
+            var texts = GetComponentsInChildren<TMP_Text>();
+            time = texts[0];
+        }
+
+        void Update()
+        {
+            TimeLimit -= Time.deltaTime;
+
+            // 残り秒数が0になったとき用
+            if (TimeLimit <= 0.0)
+            {
+                TimeLimit = 0.0f;
+            }
+
+            time.text = TimeLimit.ToString("F2");
+        }
+
+    }
+}
