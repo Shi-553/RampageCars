d63ac040f9e912621652f6eaa476b01c7ff6d33d
竹内廉 <ren02157003nao@gmail.com>
2022-05-04 03:53:11 +0900

    プルするためいったんプッシュ

---
+++ Assets/Scenes/UIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RampageCars
+{
+    [RequireComponent(typeof(SpeedMeasure))]
+    public class UIController : MonoBehaviour
+    {
+        //UIAction action;
+        //UIMover ui;
+        //UIActionManager actionManager;
+
+        //private void OnEnable()
+        //{
+        //    action = new();
+        //    action.Enable();
+        //}
+
+        //private void OnDisable()
+        //{
+        //    action.Disable();
+        //}
+
+        //void Start()
+        //{
+        //    actionManager = GetComponent<UIActionManager>();
+        //    action.UI.Submit.performed;
+        //    action.UI.Move.performed;
+
+        //}
+
+        
+        //void Update()
+        //{
+        
+        //}
+    }
+}
