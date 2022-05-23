546621e3382cc0c74dec62c60720439c49a3c874
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-05-23 17:02:34 +0900

    超簡易的なゴールを作った。 暫定でゲームオーバーに遷移する。

---
+++ Assets/Objects/Goal/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RampageCars
+{
+    public class Goal : MonoBehaviour
+    {
+        [SerializeField]
+        SceneType clearScene;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<IPlayerTag>() != null)
+            {
+                Singleton.Get<SceneLoader>().ChangeScene(clearScene);
+            }
+        }
+    }
+}
