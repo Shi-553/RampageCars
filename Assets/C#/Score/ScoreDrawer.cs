8984ea2d7f98071e7c06806fde3937ae06b6baf8
momonga2525 <arst.0525@gmail.com>
2022-05-24 17:01:19 +0900

    撃破数を画面右端に追加しました。

---
+++ Assets/C#/Score/ScoreDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace RampageCars
+{
+    public class ScoreDrawer : MonoBehaviour
+    {
+        private TMP_Text scoreLabel;
+
+        void Start()
+        {
+            scoreLabel = GetComponent<TMP_Text>();
+            scoreLabel.text = "SCORE:" + Singleton.Get<ScoreManager>().Score;
+        }
+
+        private void Update()
+        {
+            scoreLabel.text = "SCORE:" + Singleton.Get<ScoreManager>().Score;
+        }
+
+    }
+}
