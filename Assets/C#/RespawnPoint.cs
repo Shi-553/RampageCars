dae9fb8ed5f437f3e393d99a429296884bbf1ee7
toiwd <toiwd.hal@gmail.com>
2022-06-28 18:00:36 +0900

    スポーンポイントの仕様変更

---
+++ Assets/C#/RespawnPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RampageCars
+{
+    public class RespawnPoint : MonoBehaviour
+    {
+        public Vector3 Position => transform.position;
+        public Quaternion Rotation => transform.rotation;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawCube(GetComponent<BoxCollider>().center, GetComponent<BoxCollider>().size);
+
+        }
+#endif
+    }
+}
--- Assets/Objects/Player/PlayerFall.cs
+++ Assets/Objects/Player/PlayerFall.cs
@@ -7,44 +7,37 @@ namespace RampageCars
     public class PlayerFall : MonoBehaviour
     {
         [SerializeField]
-        private Transform self;
-        
-        
+        GroundChecker groundChecker;
+        [SerializeField]
+        private RespawnPoint respawnPoint;
+
         void Start()
         {
-            GetComponent<ISubscribeable<FallSeaInfo>>().Subscribe(Respawn);
+            GetComponentInChildren<ISubscribeable<FallSeaInfo>>().Subscribe(Respawn);
         }
 
 
         void Respawn(FallSeaInfo _)
         {
-            self.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-            var nearObject = FetchNearObjectWithTag("Spawn");
-            self.LookAt(nearObject);
-            transform.parent.position = nearObject.position;
-
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            if (respawnPoint == null)
+            {
+                Debug.LogError("リスポーンポイントが設定されていません。");
+                return;
+            }
+            transform.rotation = respawnPoint.Rotation;
+            transform.position = respawnPoint.Position;
         }
 
-        // １番近いオブジェクトを取得する
-        private Transform FetchNearObjectWithTag(string tagName)
+        void OnTriggerStay(Collider other)
         {
-            // 該当タグが1つしか無い場合はそれを返す
-            var targets = GameObject.FindGameObjectsWithTag(tagName);
-            if (targets.Length == 1) return targets[0].transform;
-
-            GameObject result = null;
-            var minTargetDistance = float.MaxValue;
-            foreach (var target in targets)
+            if (groundChecker.IsGrounded)
             {
-                // 前回計測したオブジェクトよりも近くにあれば記録
-                var targetDistance = Vector3.SqrMagnitude(transform.position - target.transform.position);
-                if (!(targetDistance < minTargetDistance)) continue;
-                minTargetDistance = targetDistance;
-                result = target.transform.gameObject;
+                if (other.CompareTag("Spawn"))
+                {
+                    respawnPoint = other.GetComponent<RespawnPoint>();
+                }
             }
-
-            // 最後に記録されたオブジェクトを返す
-            return result?.transform;
         }
     }
 }
