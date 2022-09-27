48b44c5911487c3daf1be98615fbea16c393e80b
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-09-27 16:19:28 +0900

    いろいろエフェクト追加

---
+++ Assets/Objects/Player/PlayerDamageEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RampageCars
+{
+    public class PlayerDamageEffect : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject effect;
+
+        [SerializeField]
+        Transform effectParent;
+
+        [SerializeField]
+        Material damageMat;
+        [SerializeField]
+        Material defaultMat;
+
+        [SerializeField]
+        SkinnedMeshRenderer damageSkinnedMeshRenderer;
+
+        void Start()
+        {
+            defaultMat =damageSkinnedMeshRenderer.sharedMaterial;
+            GetComponent<ISubscribeable<DamageInfo>>().Subscribe(info=>StartCoroutine(OnDamage(info)));
+        }
+        IEnumerator OnDamage(DamageInfo info)
+        {
+            Instantiate(effect, effectParent);
+            damageSkinnedMeshRenderer.sharedMaterial = damageMat;
+            yield return new WaitForSeconds(0.7f);
+
+            damageSkinnedMeshRenderer.sharedMaterial = defaultMat;
+        }
+
+    }
+}
